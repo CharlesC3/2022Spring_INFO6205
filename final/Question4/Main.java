267c9b657926f3e73d1ef2017aecc6303fbb2d2f
CharlesC3 <90739176+CharlesC3@users.noreply.github.com>
2022-05-06 11:06:32 -0700

    Add files via upload

---
+++ final/Question4/Main.java
@@ -0,0 +1,34 @@
+package Question4;
+
+public class Main {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(6);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(4);
+        root.left.right.left = new TreeNode(3);
+        root.left.right.right = new TreeNode(5);
+        root.right = new TreeNode(8);
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(9);
+        System.out.println(lowestCommonAncestor(root, root.left, root.left.right).val);
+        System.out.println(lowestCommonAncestor(root, root.left, root.right).val);
+    }
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        if (root == p || root == q) return root;
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        if (left == null && right == null) {
+            return null;
+        }
+        return left == null ? right : left;
+    }
+}
+
+
+
